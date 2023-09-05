6d025f4848b20e571e131c8ac0b58fcf6f23f162
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-09-05 22:54:26 +0530

    Update MyButton2.jsx

--- FirstOne/Buttons/MyButton2.jsx
+++ FirstOne/Buttons/MyButton2.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 const MyButton2 = (props) => {
   return (
-    <div>MyButton2
-            <button onClick={()=>props.onPress("this msg is using component my button")}>click me</button>
-
+    <div>
+          <button onClick={()=>props.onPress("this msg is using component my button")}>click me</button>
     </div>
   )
 }
