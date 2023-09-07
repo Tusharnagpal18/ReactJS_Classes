b72d5d7fe000b8da03bc73b7d4858eb3ab7eaef9
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-09-07 20:58:31 +0530

    Add files via upload

---
+++ Todo/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./WatchPage/style.css";
+// import Mytodo from "./Todo/Mytodo";
+import Home from "./WatchPage/Home";
+const App = () => {
+  return (
+    <>
+      <body>
+        {/* <Mytodo /> */}
+        <Home />
+
+        {/* <h1 className='heading_style'>Top 5 Netflix Series</h1> */}
+        {/* {MyData.map((val,index)=>(
+      <MyCard
+        key={index}
+        title={val.title}
+        seriesName={val.seriesName}
+        link={val.link}
+        img={val.imgsrc} 
+         />     
+     ))} */}
+      </body>
+    </>
+  );
+};
+
+export default App;
---
+++ Todo/Todo/Mytodo.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useSyncExternalStore } from "react";
+import "./style.css";
+const Mytodo = () => {
+  const [activity, setActivity] = useState("");
+  const [listData, setListData] = useState([]);
+  const addTask = () => {
+    // will work synchronously
+
+    setListData((listData) => {
+      let updList
+      if(activity.length>=1){
+        updList= [...listData, activity];
+        console.log(updList);
+        console.log(activity.length);
+        return updList;
+      }
+      else{
+        alert('please add a TODO');
+        return "";
+      }
+    });
+    setActivity("");
+  };
+  const removeTask = (i) => {
+    const updList = listData.filter((element, id) => {
+      return i != id;
+    });
+    setListData(updList);
+  };
+  const deleteAll = () => {
+    setListData([]);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="header bg-dark bg-gradient">TODO LIST</div>
+        <input
+          type="text"
+          placeholder="Add Your TODO"
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+        />
+        <button
+          onClick={addTask}
+          className="btn btn-warning  m-3 px-4 fw-bolder"
+        >
+          Add
+        </button>
+        <h4 className=" badge bg-primary fs-5 d-inline-block mb-5">
+          Here's Your TODO list 🌟📰{" "}
+        </h4>
+        {listData != [] &&
+          listData.map((data, index) => (
+            <div key={index} className="d-flex justify-content-center">
+              <div
+                className="mydiv1 bg-success bg-gradient p-2 fw-bold m-2 text-wrap"
+                style={{ marginRight: "10px", color: "black" }}
+              >
+                {data}
+              </div>
+              <div>
+              <button
+                className="btn btn-danger m-2"
+                onClick={() => removeTask(index)}
+              >
+                Delete
+              </button>
+              </div>
+            </div>
+          ))}
+        {listData.length >= 1 && (
+          <button onClick={deleteAll} className="btn btn-dark mb-3">
+            Delete All
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Mytodo;
